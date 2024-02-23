23aa5bae190f96670c50081bf3c34df6e9769cfc
umitakbas82 <umit.akbas@hotmail.com.tr>
2024-02-23 17:32:36 +0300

    IProductRepository added

---
+++ SharedLibrary/ProductRepository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using SharedLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.ProductRepository
+{
+    public interface IProductRepository
+    {
+        Task<Product> AddProductAsync(Product model);
+        Task<Product> UpdateProductAsync(Product model);
+        Task<Product> DeleteProductAsync(int productId);
+        Task<List<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(int productId);
+    }
+}
