398e0482f377007330ab7f750040b3972a730404
umitakbas82 <umit.akbas@hotmail.com.tr>
2024-02-23 17:56:26 +0300

    Implamentitations added

---
+++ BlazorCrudApp/BlazorCrudApp/Implementations/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using BlazorCrudApp.Data;
+using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Models;
+using SharedLibrary.ProductRepository;
+
+namespace BlazorCrudApp.Implementations
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext appDbContext;
+        public ProductRepository(AppDbContext appDbContext) 
+        {
+            this.appDbContext = appDbContext;        
+        }
+
+        public async Task<Product> AddProductAsync(Product model)
+        {
+            if (model is null) return null!;
+           
+            var chk = await appDbContext.Products.Where(_ => _.Name.ToLower().Equals(model.Name.ToLower())).FirstOrDefaultAsync();
+            if(chk is null)return null!;
+           var newDataAdded= appDbContext.Products.Add(model).Entity;
+            await appDbContext.SaveChangesAsync();
+            return newDataAdded;
+        }
+
+        public Task<Product> DeleteProductAsync(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByIdAsync(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> UpdateProductAsync(Product model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
