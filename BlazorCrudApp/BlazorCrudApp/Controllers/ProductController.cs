a31af17ab59489a8af7785d10e9614e45ef942ab
UmitAkbas <umit.akbas@hotmail.com.tr>
2024-02-25 02:08:55 +0300

    contoller ready

--- BlazorCrudApp/BlazorCrudApp/Controllers/ProductController.cs
+++ BlazorCrudApp/BlazorCrudApp/Controllers/ProductController.cs
@@ -12,9 +12,44 @@ namespace BlazorCrudApp.Controllers
         private readonly IProductRepository productRepository;
         public ProductController(IProductRepository productRepository)
         {
-            this.productRepository=productRepository;
+            this.productRepository = productRepository;
         }
         [HttpGet("All-Products")]
-        public async Task<ActionResult<List<Product>>>
+        public async Task<ActionResult<List<Product>>> GetAllProductAsync()
+        {
+            var products = await productRepository.GetAllProductsAsync();
+            return Ok(products);
+        }
+        [HttpGet("Single-Product/{id}")]
+        public async Task<ActionResult<List<Product>>> GetSingleProductAsync(int id)
+        {
+            var product = await productRepository.GetProductByIdAsync(id);
+            return Ok(product);
+
+        }
+
+        [HttpPost("Add-Product")]
+        public async Task<ActionResult<List<Product>>> AddProductAsync(Product model)
+        {
+            var product = await productRepository.AddProductAsync(model);
+            return Ok(product);
+
+        }
+
+        [HttpPut("Update-Product")]
+        public async Task<ActionResult<List<Product>>> UpdateProductAsync(Product model)
+        {
+            var product = await productRepository.UpdateProductAsync(model);
+            return Ok(product);
+
+        }
+        [HttpDelete("Delete-Product /{id}")]
+        public async Task<ActionResult<List<Product>>> DeleteProductAsync(int id)
+        {
+            var product = await productRepository.DeleteProductAsync(id);
+            return Ok(product);
+
+        }
+
     }
 }
