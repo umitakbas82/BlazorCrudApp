e2bb479be8ff6a77a13febbc951dcf9aa39305dd
umitakbas82 <umit.akbas@hotmail.com.tr>
2024-02-26 17:45:08 +0300

    ManageProduct and ProductList Pages Added

--- SharedLibrary/Models/Product.cs
+++ SharedLibrary/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿ using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace SharedLibrary.Models
     public class Product
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public int Quantity { get; set; }
     }
 }
